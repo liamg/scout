b264b53efbbe134ea20da02e466241d1e1502b6e
Liam Galvin <liam@liam-galvin.co.uk>
2020-01-12 18:25:43 +0000

    Add supplementary checks for backup extensions

--- pkg/scan/url_scanner_test.go
+++ pkg/scan/url_scanner_test.go
@@ -32,7 +32,8 @@ func TestURLScanner(t *testing.T) {
 
 	scanner := NewURLScanner(&URLOptions{
 		TargetURL:   *parsed,
-		Parallelism: 100,
+		Parallelism: 2,
+		Wordlist:    wordlist.FromReader(bytes.NewReader([]byte("login.php\nsomething.php"))),
 	})
 
 	results, err := scanner.Scan()
@@ -40,7 +41,7 @@ func TestURLScanner(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	require.Equal(t, len(results), 1)
+	require.Equal(t, 1, len(results))
 	assert.Equal(t, results[0].String(), server.URL+"/login.php")
 
 }
@@ -66,7 +67,7 @@ func TestURLScannerWithRedirects(t *testing.T) {
 
 	scanner := NewURLScanner(&URLOptions{
 		TargetURL:           *parsed,
-		Parallelism:         100,
+		Parallelism:         1,
 		PositiveStatusCodes: []int{http.StatusOK},
 		Wordlist:            wordlist.FromReader(bytes.NewReader([]byte("login.php"))),
 	})
@@ -76,7 +77,43 @@ func TestURLScannerWithRedirects(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	require.Equal(t, len(results), 1)
+	require.Equal(t, 1, len(results))
 	assert.Equal(t, results[0].String(), server.URL+"/very-secret-file.php")
+
+}
+
+func TestURLScannerWithBackupFile(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/login.php":
+			w.WriteHeader(http.StatusOK)
+		case "/login.php~":
+			w.WriteHeader(http.StatusOK)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	defer server.Close()
+
+	parsed, err := url.Parse(server.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	scanner := NewURLScanner(&URLOptions{
+		TargetURL:   *parsed,
+		Parallelism: 1,
+		Wordlist:    wordlist.FromReader(bytes.NewReader([]byte("login.php"))),
+	})
+
+	results, err := scanner.Scan()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, 2, len(results))
+	assert.Equal(t, results[0].String(), server.URL+"/login.php")
+	assert.Equal(t, results[1].String(), server.URL+"/login.php~")
 
 }
