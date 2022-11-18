0ddf66413b029347a796856d2226b7a32beb3ae9
Owen Rumney <owen.rumney@ghost.security>
2022-11-18 17:33:25 +0000

    feat: update the wordlist addition

--- go.mod
+++ go.mod
@@ -8,13 +8,5 @@ require (
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 	github.com/stretchr/testify v1.3.0
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 )
--- scripts/import-wordlists.sh
+++ scripts/import-wordlists.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-go get -u github.com/go-bindata/go-bindata/...
+set -x
+
+go install github.com/go-bindata/go-bindata/go-bindata@latest
 go-bindata -o internal/app/scout/data/wordlists.go assets/
-sed -i 's/package main/package data/g' internal/app/scout/data/wordlists.go
+cp internal/app/scout/data/wordlists.go internal/app/scout/data/wordlists.go.old
+sed -e 's/package main/package data/g' internal/app/scout/data/wordlists.go.old > internal/app/scout/data/wordlists.go
+
+rm internal/app/scout/data/wordlists.go.old
